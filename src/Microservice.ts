df90fe9faac877b9d4103825d8b1d8bc909c87c1
Cristi <cristimc8@gmail.com>
2023-03-11 15:06:18 +0200

    chore: add scanphish as a Microservice

--- src/Microservice.ts
+++ src/Microservice.ts
@@ -1,22 +1,26 @@
 export enum Microservice {
   Authphish = 'authphish',
-  Storephish = 'storephish'
+  Storephish = 'storephish',
+  Scanphish = 'scanphish'
 }
 
 export const InternalPaths = {
   [Microservice.Authphish]: `http://authphish:${process.env.AUTHPHISH_PORT}`,
   [Microservice.Storephish]: `http://storephish:${process.env.STOREPHISH_PORT}`,
+  [Microservice.Scanphish]: `http://scanphish:${process.env.SCANPHISH_PORT}`,
 } as const;
 
 export const DeployedPaths = {
   [Microservice.Authphish]: 'https://auth.starphish.app',
   [Microservice.Storephish]: 'https://store.starphish.app',
+  [Microservice.Scanphish]: 'https://scan.starphish.app',
 } as const;
 
 /* eslint-disable max-len */
 export const LocalPaths = {
   [Microservice.Authphish]: `http://localhost:${process.env.AUTHPHISH_PORT || process.env.REACT_APP_AUTHPHISH_PORT}`,
   [Microservice.Storephish]: `http://localhost:${process.env.STOREPHISH_PORT || process.env.REACT_APP_STOREPHISH_PORT}`,
+  [Microservice.Scanphish]: `http://localhost:${process.env.SCANPHISH_PORT || process.env.REACT_APP_SCANPHISH_PORT}`,
 } as const;
 /* eslint-enable max-len */
 
