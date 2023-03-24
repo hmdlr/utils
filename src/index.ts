3c3a70731df3fd56f461cbae21d7fc84575125b9
Cristi <cristimc8@gmail.com>
2023-03-24 15:27:15 +0200

    chore: manually set container instance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Container } from 'typedi';
 import { getInternalClient, InternalStarphishClient } from './client/StarphishClient';
 import { UnauthorizedError } from './errors/UnauthorizedError';
 import { kafkaConsumeMessage } from './kafka/Handler';
@@ -5,6 +6,11 @@ import { getLogger } from './Logger';
 import { DeployedPaths, FrontPaths, LocalPaths } from './Microservice';
 import { extractAuth, isAuth } from './middleware/isAuth';
 import Storephish from './sdk/Storephish';
+
+Container.set(
+  'storephish',
+  Storephish
+);
 
 export {
   getLogger,
