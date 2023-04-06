31d713f62bbb38952c20044583826e02acffd215
Cristi <cristimc8@gmail.com>
2023-04-07 00:04:30 +0300

    feat: add brand operations on scanphish SDK

--- src/sdk/Scanphish.ts
+++ src/sdk/Scanphish.ts
@@ -1,4 +1,4 @@
-import { BareClient, Collection } from '@hmdlr/types';
+import { BareClient, Collection, IBrand, IBrandCreatePayload } from '@hmdlr/types';
 
 export default class Scanphish {
   private readonly api = 'api';
@@ -7,12 +7,33 @@ export default class Scanphish {
     private client: BareClient
   ) {}
 
+  /* ===================== */
+  /* Scans */
   public async scan(
     collection: Collection
   ): Promise<any> {
     return this.client.post<any>(
       `${this.api}/scan`,
       collection
+    );
+  }
+
+  /* ===================== */
+  /* Brands */
+  public async enhanceBrand(brandId: string): Promise<{
+    candidates: string[]
+  }> {
+    return this.client.get<{
+      candidates: string[]
+    }>(
+      `${this.api}/brand/enhance/${brandId}`
+    );
+  }
+
+  public async createBrand(brand: IBrandCreatePayload): Promise<IBrand> {
+    return this.client.post<IBrand>(
+      `${this.api}/brand`,
+      brand
     );
   }
 }
