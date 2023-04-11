1e3c04abf922d8ff45bd72a7ead90008192f1b8b
Cristi <cristimc8@gmail.com>
2023-04-11 22:23:56 +0300

    feat: add preset method calls to sdk

--- src/sdk/Scanphish.ts
+++ src/sdk/Scanphish.ts
@@ -47,9 +47,47 @@ export default class Scanphish {
     );
   }
 
+  /* ===================== */
+  /* Configs */
+
+  /**
+   * Returns a list of all the configs the user has access to
+   * @param request
+   * @param includeBrands
+   */
   public async listConfigs(request: PagedRequest, includeBrands = false) {
     return this.client.get<PagedResults<IConfig>>(
       `${this.configsApi}/?includeBrands=${includeBrands}&${buildPagedRequest(request)}`
     ).then(PagedResults.fromPagedJson as any);
+  }
+
+  /**
+   * Returns a list of all the active configs the user has access to
+   */
+  public async listPresets() {
+    return this.client.get<IConfig[]>(
+      `${this.configsApi}/preset`
+    );
+  }
+
+  /**
+   * Adds a config to the list of active configs
+   * @param configId
+   */
+  public async savePreset(configId: string) {
+    return this.client.post<void>(
+      `${this.configsApi}/preset`,
+      { configId }
+    );
+  }
+
+  /**
+   * Removes a config from the list of active configs
+   * @param configId
+   */
+  public async deletePreset(configId: string) {
+    return this.client.delete<void>(
+      `${this.configsApi}/preset/${configId}`
+    );
   }
 }
